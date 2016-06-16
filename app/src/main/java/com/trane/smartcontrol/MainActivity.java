3783658779e9f4f2e44896b5e0cb463388fba0cc
sangbomkoh <sangbomkoh@gmail.com>
2016-06-15 19:30:30 -0500

    init

---
+++ app/src/main/java/com/trane/smartcontrol/MainActivity.java
@@ -0,0 +1,20 @@
+package com.trane.smartcontrol;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView weatherImage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // add weather image
+        weatherImage = (ImageView) findViewById(R.id.imageView);
+        weatherImage.setImageResource(R.drawable.sunshine);
+    }
+}
