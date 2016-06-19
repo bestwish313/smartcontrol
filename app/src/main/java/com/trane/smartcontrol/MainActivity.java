319ab4a893280417788ca18938c41a6cd4d0481f
sangbomkoh <sangbomkoh@gmail.com>
2016-06-18 23:46:53 -0500

    bottom menu

--- app/src/main/java/com/trane/smartcontrol/MainActivity.java
+++ app/src/main/java/com/trane/smartcontrol/MainActivity.java
@@ -1,7 +1,9 @@
 package com.trane.smartcontrol;
 
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,5 +18,19 @@ public class MainActivity extends AppCompatActivity {
         // add weather image
         weatherImage = (ImageView) findViewById(R.id.imageView);
         weatherImage.setImageResource(R.drawable.sunshine);
+
+        // bottom mode selector
+        View v = this.findViewById(android.R.id.content);
+        v.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Heating   Cooling   Fan  Stop", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    public ImageView getWeatherImage() {
+        return weatherImage;
     }
 }
