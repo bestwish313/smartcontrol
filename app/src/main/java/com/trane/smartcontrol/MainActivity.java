417e18315cb42a0be1b4df5d1edfc4d3157fbd3f
sangbomkoh <sangbomkoh@gmail.com>
2016-06-21 14:11:54 -0500

    test

--- app/src/main/java/com/trane/smartcontrol/MainActivity.java
+++ app/src/main/java/com/trane/smartcontrol/MainActivity.java
@@ -4,12 +4,15 @@ import android.os.Bundle;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     ImageView weatherImage;
     private Gauge gauge;
+    int i = 50;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,7 +34,55 @@ public class MainActivity extends AppCompatActivity {
         });
 
         gauge = (Gauge) findViewById(R.id.gauge1);
-        gauge.setValue(70);
+        gauge.setValue(50);
+        final TextView tempDisplay = (TextView) findViewById(R.id.textView15);
+        tempDisplay.setText(Integer.toString(gauge.getValue()));
+        final TextView statusDisplay = (TextView)findViewById(R.id.textView14);
+
+        // temperature buttons setup
+        ImageButton upButton = (ImageButton) findViewById(R.id.button6);
+        upButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                if (i < 91) {
+                    gauge.setValue(i++);
+                    tempDisplay.setText(Integer.toString(gauge.getValue()));
+                }
+            }
+        });
+
+        ImageButton downButton = (ImageButton) findViewById(R.id.button7);
+        downButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                if (i > 49) {
+                    gauge.setValue(i--);
+                    tempDisplay.setText(Integer.toString(gauge.getValue()));
+                }
+            }
+        });
+
+        ImageButton heatButton = (ImageButton) findViewById(R.id.button5);
+        heatButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                statusDisplay.setTextColor(getResources().getColor(R.color.md_red_500));
+                statusDisplay.setText("HEAT DEMAND");
+            }
+        });
+
+        ImageButton coolButton = (ImageButton) findViewById(R.id.button4);
+        coolButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                statusDisplay.setTextColor(getResources().getColor(R.color.md_sky_500));
+                statusDisplay.setText("COOL DEMAND");
+            }
+        });
     }
 
     public ImageView getWeatherImage() {
